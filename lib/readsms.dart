fff26d85f5db366380ced95fab06914c9aff2eaf
Ayush783 <ayushc45xxx@gmail.com>
2022-08-08 09:48:04 +0530

    changed stream type to Stream<SMS>

--- lib/readsms.dart
+++ lib/readsms.dart
@@ -1,25 +1,29 @@
 import 'dart:async';
-
+export 'model/sms.dart';
 import 'package:flutter/services.dart';
+import 'package:readsms/model/sms.dart';
+export 'model/sms.dart';
 
 class Readsms {
   /// Event channel used for setting up the stream to receive
   /// sms continously
   static const _channel = EventChannel("readsms");
 
-  final StreamController _controller = StreamController();
+  final StreamController _controller = StreamController<SMS>();
 
-  /// The stream containing sms as String
-  Stream get smsStream => _controller.stream;
+  /// The stream containing sms as [SMS] objects
+  Stream<SMS> get smsStream => _controller.stream as Stream<SMS>;
 
   late final StreamSubscription _channelStreamSubscription;
 
   /// Listens to the broadcast stream exposed by the event
-  /// channel and adds data on the [smsStream] whenever
+  /// channel and adds [SMS] on the [smsStream] whenever
   /// sms is received
   read() {
     _channelStreamSubscription = _channel.receiveBroadcastStream().listen((e) {
-      if (!_controller.isClosed) _controller.sink.add(e);
+      if (!_controller.isClosed) {
+        _controller.sink.add(SMS.fromList(e));
+      }
     });
   }
 
