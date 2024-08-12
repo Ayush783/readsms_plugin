9e70efb5ad0da04911dce58cde86e0cb60bd4cc8
Roman Matroskin <romatroskin@gmail.com>
2024-08-12 17:24:51 +0200

    Update ReadsmsPlugin.kt
    missing import

--- android/src/main/kotlin/com/ayush783/readsms/ReadsmsPlugin.kt
+++ android/src/main/kotlin/com/ayush783/readsms/ReadsmsPlugin.kt
@@ -10,6 +10,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Build
+import android.telephony.SmsMessage
 import android.provider.Telephony
 import android.util.Log
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
